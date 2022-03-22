12611ed0ba219f49bcdb03a86bd83afbd987535a
Khwan <khwansila@hotmail.com>
2022-03-22 08:42:30 +0700

    update at 21 mar 2022

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // dd(DB::Table('tb_admins')->get());
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+
 Auth::routes();
 
 Route::post('loginUser', 'Auth\LoginController@login')->name('loginUser');
