079fa384a1b9efc540779c225702c56854a5fab9
N-Daoloet <62612351+N-Daoloet@users.noreply.github.com>
2022-04-11 17:42:32 +0700

    nes update

--- app/Http/Controllers/admin/FamilyController.php
+++ app/Http/Controllers/admin/FamilyController.php
@@ -11,11 +11,7 @@ use App\Models\Family;
 
 class FamilyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function index()
     {
         $Family = Family::get();
@@ -23,22 +19,7 @@ class FamilyController extends Controller
         return view('backend.family.familylist', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         dd($request->all());
@@ -66,23 +47,13 @@ class FamilyController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function edit($id)
     {
         $Family = Family::findOrFail($id);
@@ -90,13 +61,6 @@ class FamilyController extends Controller
         return view('backend.family.editfamily', $data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         // dd($request->all());
@@ -129,12 +93,7 @@ class FamilyController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy($id)
     {
         DB::beginTransaction();
