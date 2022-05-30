f1f259830698af5ed37f94bc23351d7fbe00fd89
N-Daoloet <62612351+N-Daoloet@users.noreply.github.com>
2022-05-30 17:36:19 +0700

    .

--- app/Http/Controllers/merchant/OrderMerchantController.php
+++ app/Http/Controllers/merchant/OrderMerchantController.php
@@ -122,7 +122,7 @@ class OrderMerchantController extends Controller
             $content = curl_exec( $ch );
             curl_close($ch);
             $json = json_decode($content);
-            dd($json);
+            dd($content);
             $jsondata = (array)$json->data;
             if($json->status === 'false'){
                 // dd('whattttttt');
