e58ad7b3ce113e9dfe5f863c4c729e9da0e71bb5
N-Daoloet <62612351+N-Daoloet@users.noreply.github.com>
2022-08-16 20:25:59 +0700

    .

--- resources/views/mobile/member/common/content/store.blade.php
+++ resources/views/mobile/member/common/content/store.blade.php
@@ -121,6 +121,12 @@
         <img class="justify-content-center w-100" src="{{('https://testgit.sapapps.work/moldii/storage/app/banner/'.$ban->banner_name.'')}}"  alt="alt">
         <div class="col-12 p-2 row justify-content-center m-0">
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <div class="text-center " >
@@ -140,14 +146,19 @@
 
                     @endforeach
 
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
+                   
 
                     
                 </div>
+
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
                     
             </div>
 
