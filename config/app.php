ba085f1deceafb07ec394e966d542a91980b8ec3
N-Daoloet <62612351+N-Daoloet@users.noreply.github.com>
2022-08-18 21:46:38 +0700

    .

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
-        
+        Illuminate\View\ViewServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -231,6 +231,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        
 
     ],
 
